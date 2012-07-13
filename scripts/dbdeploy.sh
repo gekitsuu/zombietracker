26b455f4301bc1038f696106449aeef28e689a8e
Adam Glenn <adam.glenn@rackspace.com>
2012-07-13 10:12:20 -0400

    Updated deploy scripts

--- scripts/dbdeploy.sh
+++ scripts/dbdeploy.sh
@@ -3,4 +3,4 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" > /etc/apt/sources.list.d/10gen.list
 
-apt-get update && apt-get upgrade -y && apt-get install mongodb-10gen
+apt-get update && apt-get upgrade -y && apt-get install -y mongodb-10gen
--- scripts/wwwdeploy.sh
+++ scripts/wwwdeploy.sh
@@ -1,15 +1,16 @@
 apt-get update
 apt-get upgrade -y
-apt-get -y install apache2 wget git python-stuptools libapache2-mod-wsgi 
+apt-get -y install apache2 wget git python python-setuptools libapache2-mod-wsgi 
 
 easy_install pip
 pip install flask pymongo virtualenv virtualenvwrapper
 
-su zombietracker -c 'mkvirtualenv zombietracker'
-
 # Create the zombietracker user for our app
-useradd zombietracker
+useradd -m -s /bin/bash zombietracker
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/zombietracker/.bashrc
+su zombietracker -c 'mkvirtualenv zombietracker'
 
 cd /opt
 git clone http://github.com/gekitsuu/zombietracker
 chown -R zombietracker:www-data /opt/zombietracker
+
