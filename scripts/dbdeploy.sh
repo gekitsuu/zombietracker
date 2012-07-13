a1bc420b05af382f6952f4b8d6107647e2c77988
Adam Glenn <adam.glenn@rackspace.com>
2012-07-13 16:26:08 -0400

    Setting up the scraper.py script

--- scripts/dbdeploy.sh
+++ scripts/dbdeploy.sh
@@ -3,4 +3,10 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" > /etc/apt/sources.list.d/10gen.list
 
-apt-get update && apt-get upgrade -y && apt-get install -y mongodb-10gen
+apt-get update
+apt-get upgrade -y
+apt-get install -y mongodb-10gen python python-setuptools git
+easy_install pip
+pip install BeautifulSoup
+
+git clone http://github.com/gekitsuu/zombietracker
