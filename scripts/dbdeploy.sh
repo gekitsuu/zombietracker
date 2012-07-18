98c6550a4109f0bc5764983c0bb523cb186c36b1
Adam Glenn <adam.glenn@rackspace.com>
2012-07-17 20:04:01 -0400

    Comments and fixes

--- scripts/dbdeploy.sh
+++ scripts/dbdeploy.sh
@@ -1,25 +1,29 @@
-# Install 10gen's Key
+# Install MongoDB
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-
 echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" > /etc/apt/sources.list.d/10gen.list
 
+# Update the system
 apt-get update
 apt-get upgrade -y
-apt-get install -y mongodb-10gen python python-setuptools git
+apt-get install -y mongodb-10gen python python-setuptools git salt pyzmq
 easy_install pip
 pip install BeautifulSoup pymongo
 
+# Pull Down Zombie Tracker
 git clone http://github.com/gekitsuu/zombietracker
 
+# Lockdown Mongo
 mongo /root/zombietracker/scripts/lockdown.js
-
-echo "auth = True" >> /etc/mongodb.conf
+sed -i -e "s!#auth =True!auth = True!" /etc/mongodb.conf
 service mongodb restart
+
+# Populate The Database
 
 DBIP=`ifconfig eth0|grep 'inet addr'|awk -F':' '{print $2}'|awk '{print $1}'`
 USERNAME=EliteZombieTracker
 PASSWORD=impossiblepassword
 
 sed -i -e "s!%IP%!$USERNAME:$PASSWORD@$DBIP!" /root/zombietracker/configs/dbcreds.json
-
-python /root/zombietracker/scripts/scraper.py
+cd /root/zombietracker/
+pip install ./
+python scripts/scraper.py
--- scripts/wwwdeploy.sh
+++ scripts/wwwdeploy.sh
@@ -1,21 +1,23 @@
+# Update teh system
 apt-get update
 apt-get upgrade -y
+
+# Install Necessary Packages
 apt-get -y install apache2 wget git python python-setuptools libapache2-mod-wsgi 
 
+# Install Python Package
 easy_install pip
-pip install virtualenv virtualenvwrapper
+pip install virtualenv virtualenvwrapper salt
 
 # Create the zombietracker user for our app
 useradd -m -s /bin/bash zombietracker
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/zombietracker/.bashrc
 su zombietracker -c 'source /usr/local/bin/virtualenvwrapper.sh;mkvirtualenv zombietracker'
-
 cd /home/zombietracker 
 git clone http://github.com/gekitsuu/zombietracker
-
 cp /home/zombietracker/zombietracker/configs/apache.conf /etc/apache2/sites-available/default
-
 su zombietracker -c '/home/zombietracker/.virtualenvs/zombietracker/bin/pip install flask pymongo'
 su - zombietracker -c 'cd /home/zombietracker; source /home/zombietracker/.virtualenvs/zombietracker/bin/activate; pip install ./zombietracker'
 
+# Restart Apache
 service apache2 restart
